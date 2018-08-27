f0ee441c5d0873b666aec1a84bf742504c3ae1e7
truboprovod4uk <truboprovod4uk@gmail.com>
2018-08-27 11:52:25 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function minus(){
     document.getElementById("result").value = Number(fn) - Number(sn);
 }
 
-function multiple(){
+function multiply(){
 	var fn = document.getElementById("fn").value;
 	var sn = document.getElementById("sn").value;
     document.getElementById("result").value = Number(fn) * Number(sn);
@@ -23,8 +23,13 @@ function division(){
 }
 
 function clean(){
-
 	document.getElementById("fn").value = "";
 	document.getElementById("sn").value = "";
 	document.getElementById("result").value = "";
 }
+
+document.querySelector("#add").addEventListener("click", add);
+document.querySelector("#minus").addEventListener("click", minus);
+document.querySelector("#multiply").addEventListener("click", multiply);
+document.querySelector("#division").addEventListener("click", division);
+document.querySelector("#clean").addEventListener("click", clean);
