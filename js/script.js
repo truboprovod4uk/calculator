f1bdae57ba820326f84d6ba41da2032c4d6461d8
truboprovod4uk <truboprovod4uk@gmail.com>
2018-08-28 10:09:43 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
-function add(){
-	var fn = document.getElementById("fn").value;
-	var sn = document.getElementById("sn").value;
-    document.getElementById("result").value = Number(fn) + Number(sn);
-}
+document.addEventListener("DOMContentLoaded",
+   function (event){
 
-function minus(){
-	var fn = document.getElementById("fn").value;
-	var sn = document.getElementById("sn").value;
-    document.getElementById("result").value = Number(fn) - Number(sn);
-}
+		function add(event){
+			var fn = document.getElementById("fn").value;
+			var sn = document.getElementById("sn").value;
+			document.getElementById("result").value = Number(fn) + Number(sn);
+			console.log(event);
+		}
 
-function multiply(){
-	var fn = document.getElementById("fn").value;
-	var sn = document.getElementById("sn").value;
-    document.getElementById("result").value = Number(fn) * Number(sn);
-}
+		function minus(){
+			var fn = document.getElementById("fn").value;
+			var sn = document.getElementById("sn").value;
+			document.getElementById("result").value = Number(fn) - Number(sn);
+		}
 
-function division(){
-	var fn = document.getElementById("fn").value;
-	var sn = document.getElementById("sn").value;
-    document.getElementById("result").value = Number(fn) / Number(sn);
-}
+		function multiply(){
+			var fn = document.getElementById("fn").value;
+			var sn = document.getElementById("sn").value;
+			document.getElementById("result").value = Number(fn) * Number(sn);
+		}
 
-function clean(){
-	document.getElementById("fn").value = "";
-	document.getElementById("sn").value = "";
-	document.getElementById("result").value = "";
-}
+		function division(){
+			var fn = document.getElementById("fn").value;
+			var sn = document.getElementById("sn").value;
+			document.getElementById("result").value = Number(fn) / Number(sn);
+		}
 
-document.querySelector("#add").addEventListener("click", add);
-document.querySelector("#minus").addEventListener("click", minus);
-document.querySelector("#multiply").addEventListener("click", multiply);
-document.querySelector("#division").addEventListener("click", division);
-document.querySelector("#clean").addEventListener("click", clean);
+		function clean(){
+			document.getElementById("fn").value = "";
+			document.getElementById("sn").value = "";
+			document.getElementById("result").value = "";
+		}
+
+		document.querySelector("#add").addEventListener("click", add);
+		document.querySelector("#minus").addEventListener("click", minus);
+		document.querySelector("#multiply").addEventListener("click", multiply);
+		document.querySelector("#division").addEventListener("click", division);
+		document.querySelector("#clean").addEventListener("click", clean);
+		
+   }
+);
+
+
+
